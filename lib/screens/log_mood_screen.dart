f363a8db9b268c4de971cf3efebe76ba5dfe264d
cheeyinnn <cheeyin@1utar.my>
2026-04-13 21:08:15 +0800

    1

--- lib/screens/log_mood_screen.dart
+++ lib/screens/log_mood_screen.dart
@@ -29,7 +29,7 @@ class _LogMoodScreenState extends State<LogMoodScreen>
     {'emoji': '😞', 'label': 'Bad', 'color': Color(0xFFEF9F27)},
     {'emoji': '😐', 'label': 'Okay', 'color': Color(0xFF888780)},
     {'emoji': '😊', 'label': 'Good', 'color': Color(0xFF1D9E75)},
-    {'emoji': '😄', 'label': 'Great', 'color': Color(0xFF378ADD)},
+    {'emoji': '😄1', 'label': 'Great', 'color': Color(0xFF378ADD)},
   ];
 
   @override
